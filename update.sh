59515c12b4293290ddae568cdf48cec6991c3404
Rookie <1049825685@qq.com>
2021-01-18 14:25:19 +0800

    update

--- update.sh
+++ update.sh
@@ -1,7 +1,7 @@
 # 倉庫內hexo的備份更新
 # 如果没有消息后缀，默认提交信息为 `:pencil: update content`
 info="update" #default為$1  更改""裡面即可更改更新說明
-if ["$info" = ""]
+if ["$info" = ""];
 then info= ":pencil: update content" # ":pencil: update content"   此處為默認更新說明 if info無說明的話
 fi
 git add -A
@@ -17,7 +17,7 @@ git push origin hexo
 
 # 倉庫內master的更新 (展示給外界)
 npm = "true"
-if [ $npm = "true" ]
+if [ $npm = "true" ];
 then
     echo "Update the master!!"
     hexo clean
